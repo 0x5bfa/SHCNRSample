25fbe76154fb3c47de964af68d632d3279c614b9
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-08-26 13:01:20 +0900

    Added a rough impl of FolderWatcher

---
+++ SHCNRSample/FolderWatcher.cs
@@ -0,0 +1,116 @@
+﻿// Copyright (c) 0x5BFA.
+// Licensed under the MIT License.
+
+using System.Diagnostics;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.System.Com;
+using Windows.Win32.UI.Shell;
+using Windows.Win32.UI.Shell.Common;
+
+namespace SHCNRSample
+{
+	public unsafe class FolderWatcher
+	{
+		private uint _uRegister;
+
+		public bool StartWatching(IShellItem* psi, HWND hwnd, uint uMsg, long lEvents, BOOL fRecursive)
+		{
+			StopWatching();
+
+			ITEMIDLIST* pidlWatch;
+			HRESULT hr = PInvoke.SHGetIDListFromObject((IUnknown*)psi, &pidlWatch);
+			if (SUCCEEDED(hr))
+			{
+				SHChangeNotifyEntry entry = default;
+				entry.pidl = pidlWatch;
+				entry.fRecursive = fRecursive;
+
+				const SHCNRF_SOURCE nSources = SHCNRF_SOURCE.SHCNRF_ShellLevel | SHCNRF_SOURCE.SHCNRF_InterruptLevel | SHCNRF_SOURCE.SHCNRF_NewDelivery;
+				_uRegister = PInvoke.SHChangeNotifyRegister(hwnd, nSources, (int)lEvents, uMsg, 1, &entry);
+
+				PInvoke.CoTaskMemFree(pidlWatch);
+
+				return true;
+			}
+
+			return false;
+		}
+
+		public void StopWatching()
+		{
+			if (_uRegister is not 0U)
+			{
+				PInvoke.SHChangeNotifyDeregister(_uRegister);
+				_uRegister = 0U;
+			}
+		}
+
+		public void OnChangeMessage(WPARAM wParam, LPARAM lParam)
+		{
+			long lEvent;
+			ITEMIDLIST** rgpidl;
+			HANDLE hNotifyLock = PInvoke.SHChangeNotification_Lock((HANDLE)(nuint)wParam, (uint)(nint)lParam, &rgpidl, (int*)&lEvent);
+			if (!hNotifyLock.IsNull)
+			{
+				if (IsItemNotificationEvent(lEvent))
+				{
+					IShellItem* psi1 = null, psi2 = null;
+
+					if (rgpidl[0] is not null)
+					{
+						PInvoke.SHCreateItemFromIDList(rgpidl[0], IID.IID_IShellItem, (void**)&psi1);
+					}
+
+					if (rgpidl[1] is not null)
+					{
+						PInvoke.SHCreateItemFromIDList(rgpidl[1], IID.IID_IShellItem, (void**)&psi2);
+					}
+
+					OnChangeNotify(lEvent, psi1, psi2);
+
+					if (psi1 is not null) psi1->Release();
+					if (psi2 is not null) psi2->Release();
+				}
+				else
+				{
+					// Dispatch non-item events here in the future
+				}
+
+				PInvoke.SHChangeNotification_Unlock(hNotifyLock);
+			}
+		}
+
+		private void OnChangeNotify(long lEvent, IShellItem* psi1, IShellItem* psi2)
+		{
+			PWSTR pszLeft = null, pszRight = null;
+
+			if (psi1 is not null)
+			{
+				psi1->GetDisplayName(SIGDN.SIGDN_PARENTRELATIVE, &pszLeft);
+			}
+
+			if (psi2 is not null)
+			{
+				psi2->GetDisplayName(SIGDN.SIGDN_PARENTRELATIVE, &pszRight);
+			}
+
+			if ((SHCNE_ID)lEvent is SHCNE_ID.SHCNE_RENAMEITEM or SHCNE_ID.SHCNE_RENAMEFOLDER)
+			{
+				Debug.WriteLine($"{pszLeft} => {pszRight}");
+			}
+			else
+			{
+				Debug.WriteLine($"{pszLeft} , {pszRight}");
+			}
+
+			PInvoke.CoTaskMemFree(pszLeft);
+			PInvoke.CoTaskMemFree(pszRight);
+		}
+
+		private bool IsItemNotificationEvent(long lEvent)
+		{
+			return (lEvent & (long)(SHCNE_ID.SHCNE_UPDATEIMAGE | SHCNE_ID.SHCNE_ASSOCCHANGED | SHCNE_ID.SHCNE_EXTENDED_EVENT | SHCNE_ID.SHCNE_FREESPACE | SHCNE_ID.SHCNE_DRIVEADDGUI | SHCNE_ID.SHCNE_SERVERDISCONNECT)) is 0L;
+		}
+	}
+}
--- SHCNRSample/MainPageViewModel.cs
+++ SHCNRSample/MainPageViewModel.cs
@@ -4,13 +4,30 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.UI.Xaml.Data;
 using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.System.Com;
+using Windows.Win32.UI.Shell;
+using Windows.Win32.UI.Shell.Common;
+using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace SHCNRSample
 {
-	public class MainPageViewModel : ObservableObject
+	public unsafe class MainPageViewModel : ObservableObject
 	{
 		private ObservableCollection<ChangeLogGroupItem> GroupedItems { get; set; } = [];
 		public CollectionViewSource? ItemsGroupableSource { get => field; set => SetProperty(ref field, value); }
+
+		private const uint c_notifyMessage = PInvoke.WM_USER + 200;
+
+		[UnmanagedFunctionPointer(CallingConvention.Winapi)]
+		public delegate LRESULT WNDPROC(HWND hWnd, uint msg, WPARAM wParam, LPARAM lParam);
+
+		WNDPROC _wndProc = null!;
+
+		Dictionary<HWND, FolderWatcher> Watchers = [];
 
 		public MainPageViewModel()
 		{
@@ -24,6 +41,51 @@ namespace SHCNRSample
 			groupedItem1.Add(new ChangeLogItem() { TargetDisplayName = "DisplayName1", NotificationReason = "Reason1" });
 
 			GroupedItems.Add(groupedItem1);
+
+			SetupWatcher();
+		}
+
+		private void SetupWatcher()
+		{
+			using ComPtr<IShellItem> pShellItem = default;
+			fixed (char* pszPath = "C:\\Users\\onein\\OneDrive\\Desktop")
+				PInvoke.SHCreateItemFromParsingName(pszPath, null, IID.IID_IShellItem, (void**)pShellItem.GetAddressOf());
+
+			HWND hwnd = default;
+
+			fixed (char* pszClassName = $"FolderWatcherWindowClass{Guid.NewGuid():B}")
+			{
+				_wndProc = new WNDPROC(WndProc);
+
+				WNDCLASSEXW wndClass = default;
+				wndClass.cbSize = (uint)sizeof(WNDCLASSEXW);
+				wndClass.lpfnWndProc = (delegate* unmanaged[Stdcall]<HWND, uint, WPARAM, LPARAM, LRESULT>)Marshal.GetFunctionPointerForDelegate(_wndProc);
+				wndClass.hInstance = PInvoke.GetModuleHandle(default(PWSTR));
+				wndClass.lpszClassName = pszClassName;
+
+				PInvoke.RegisterClassEx(&wndClass);
+				hwnd = PInvoke.CreateWindowEx(0, pszClassName, null, 0, 0, 0, 0, 0, HWND.HWND_MESSAGE, default, wndClass.hInstance, null);
+			}
+
+			var watcher = new FolderWatcher();
+			watcher.StartWatching(pShellItem.Get(), hwnd, c_notifyMessage, (long)SHCNE_ID.SHCNE_ALLEVENTS, false);
+
+			Watchers.Add(hwnd, watcher);
+		}
+
+		private LRESULT WndProc(HWND hwnd, uint uMsg, WPARAM wParam, LPARAM lParam)
+		{
+			switch (uMsg)
+			{
+				case c_notifyMessage:
+					Watchers.TryGetValue(hwnd, out var watcher);
+					watcher?.OnChangeMessage(wParam, lParam);
+					break;
+				default:
+					return PInvoke.DefWindowProc(hwnd, uMsg, wParam, lParam);
+			}
+
+			return (LRESULT)0;
 		}
 	}
 }
